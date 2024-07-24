82f24bdc380b1e37d0d944ed8c8b81dd70688ea2
Mario Ochoa <mario8ato@hotmail.com>
2024-07-24 17:24:33 -0600

    Palmistry screen

--- app/src/main/java/com/mario8a/myapplication/ui/palmistry/PalmistryFragment.kt
+++ app/src/main/java/com/mario8a/myapplication/ui/palmistry/PalmistryFragment.kt
@@ -1,11 +1,16 @@
 package com.mario8a.myapplication.ui.palmistry
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import androidx.core.content.PermissionChecker
 import androidx.fragment.app.Fragment
 import com.mario8a.myapplication.databinding.FragmentPalmistryBinding
@@ -25,7 +30,7 @@ class PalmistryFragment : Fragment() {
         ActivityResultContracts.RequestPermission()
     ) { isGranted ->
         if (isGranted) {
-            // startCamera
+            startCamera()
         } else {
             Toast.makeText(
                 requireContext(),
@@ -40,11 +45,35 @@ class PalmistryFragment : Fragment() {
 
         if (checkCameraPermission()) {
             // Tiene permisos
-            // startCamera
+            startCamera()
         } else {
             requestPermissionLauncher.launch(CAMERA_PERMISSION)
         }
 
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        cameraProviderFuture.addListener({
+            val cameraProvider:ProcessCameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                cameraProvider.unbindAll()
+
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+            } catch (e: Exception) {
+                Log.i("mario8a", "Algo salio mal: ${e.message}")
+            }
+        }, ContextCompat.getMainExecutor(requireContext()))
     }
 
     private fun checkCameraPermission(): Boolean {
